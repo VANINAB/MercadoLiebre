720e447086319e8b9bf807781a8f708bbc8637db
VANINAB <87348866+VANINAB@users.noreply.github.com>
2021-08-18 19:30:40 -0300

    subida del trabajo a git

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express")
+const app = express()
+const path = require ("path")
+
+app.get ("/,", (req,res) => {
+    res.send ("hola Mundo")
+})
+
+app.listen(3002, () => {
+    console.log ("se prendio")
+})
+
+app.use (express.static(path.join(__dirname, "public")))
+
+app.get ("/", (req,res) => {
+    res.sendFile (path.join(__dirname, "./views/index.html"))
+})
+app.get ("/register", (req,res) => {
+    res.sendFile (path.join(__dirname, "./views/register.html"))
+})
+app.get ("/login", (req,res) => {
+    res.sendFile (path.join(__dirname, "./views/login.html"))
+})
